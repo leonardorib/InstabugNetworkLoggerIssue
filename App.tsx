7df4a227366a458a068787707569e9102caf4f38
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2025-03-07 19:57:45 -0300

    Created minimal app

--- App.tsx
+++ App.tsx
@@ -1,118 +1,132 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect} from 'react';
 import {
+  Alert,
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
-  View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import Instabug, {InvocationEvent, NetworkLogger} from 'instabug-reactnative';
+import NetInfo from '@react-native-community/netinfo';
+import config from './config';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  NetInfo.configure({});
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  useEffect(() => {
+    if (!config.instabugToken) {
+      console.warn('Instabug token is missing');
+    }
+    Instabug.init({
+      token: config.instabugToken,
+      invocationEvents: [InvocationEvent.twoFingersSwipe],
+    });
+    console.log('Initialized Instabug');
+  }, []);
+
+  useEffect(() => {
+    // Performs the request to NetInfo
+    NetInfo.fetch();
+  }, []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+      }}
+    >
+      <StatusBar barStyle={'dark-content'} backgroundColor="white" />
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
+        contentContainerStyle={{
+          alignItems: 'center',
+          paddingHorizontal: 24,
+        }}
+        style={{
+          flex: 1,
+          backgroundColor: 'white',
+        }}
+      >
+        <Text
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+            marginBottom: 12,
+          }}
+        >
+          Network Logger issue report
+        </Text>
+
+        <Text
+          style={{
+            marginBottom: 12,
+          }}
+        >
+          __DEV__: {`${__DEV__}`}
+        </Text>
+
+        {__DEV__ ? (
+          <Text style={{textAlign: 'center', marginBottom: 12}}>
+            ❗️ You are running the debug build. To see the error being logged
+            while in __DEV__ make sure the patch on the patches folder with logs
+            was applied after `npm install`. If not, you can apply it manually.
+          </Text>
+        ) : null}
+
+        {config.instabugToken ? null : (
+          <Text style={{color: 'red', textAlign: 'center', marginBottom: 12}}>
+            Instabug token is missing in config.ts
+          </Text>
+        )}
+
+        <Button
+          title="Show instabug"
+          onPress={() => {
+            Instabug.show();
+          }}
+        />
+
+        <Button
+          title="Enable network logger"
+          onPress={() => {
+            NetworkLogger.setEnabled(true);
+            Alert.alert('Network logger enabled');
+          }}
+        />
+
+        <Button
+          title="Disable network logger"
+          onPress={() => {
+            NetworkLogger.setEnabled(false);
+            Alert.alert('Network logger disabled');
+          }}
+        />
+
+        <Button
+          title="Simulate first request from NetInfo"
+          onPress={async () => {
+            try {
+              // By recalling .configure we are resetting the state of the NetInfo
+              // and simulating the first request after configure which is
+              // guaranteed to reproduce the error.
+              NetInfo.configure({});
+
+              await NetInfo.fetch();
+            } catch (error: any) {
+              // This wont catch the error being reported. Because the actual error is unhandled.
+              // This catch block is here only to demonstrate that you can't catch it,
+              // otherwise we would see it.
+              console.error('Error checking network state', error);
+              Alert.alert(
+                'Error checking network state',
+                error?.message || 'Something went wrong',
+              );
+            }
+          }}
+        />
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
